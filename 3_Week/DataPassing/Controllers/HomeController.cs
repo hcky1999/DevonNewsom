67a139a7cf2238662074db6aef2282dcb768dd35
Devon <=>
2019-03-25 18:58:51 -0500

    passing data

---
+++ 3_Week/DataPassing/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DataPassing.Models;
+
+namespace DataPassing.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("")]
+        // [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Name = "Devon";
+            ViewBag.Location = "Texas";
+            return View();
+        }
+
+        [HttpPost("process")]
+        public IActionResult DoForm(string firstName, string lastName, string location)
+        {
+            ViewBag.Name = $"{firstName} {lastName}";
+            ViewBag.Location = location;
+            return View("Index");
+        }
+
+    }
+}
---
+++ 3_Week/DataPassing/Models/MyModel.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace DataPassing.Models
+{
+    public class MyModel
+    {
+       
+    }
+}
