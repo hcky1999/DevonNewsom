2b402e315dbb16abf4dbe8532bc54b9eeb8fdd66
Devon <=>
2019-04-09 17:41:29 -0700

    mysql with asp

---
+++ 5_Week/MessageBoard/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MessageBoard.Models;
+using DbConnection;
+
+namespace MessageBoard.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            // query for all posts
+            List<Dictionary<string, object>> result = DbConnector.Query("SELECT * FROM posts");
+
+            ViewBag.Posts = result;
+            // provide them to my Index.cshtml
+            return View();
+        }
+        [HttpGet("new")]
+        public IActionResult New()
+        {
+            return View();
+        }
+        [HttpPost("create")]
+        public IActionResult Create(Post post)
+        {
+            if(ModelState.IsValid)
+            {
+                // we will add new post to the DB!
+                string insertQuery = $"INSERT INTO posts (Username, Topic, Content, CreatedAt, UpdatedAt) VALUES (\"{post.Username}\", \"{post.Topic}\", \"{post.Content}\", NOW(), NOW());";
+                DbConnector.Execute(insertQuery);
+                return RedirectToAction("Index");
+            }
+            // ModelState contains the error info
+            return View("New");
+        }
+    }   
+}
---
+++ 5_Week/MessageBoard/Models/Post.cs
@@ -0,0 +1,16 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MessageBoard.Models
+{
+    public class Post
+    {
+        [Required]
+        public string Username {get;set;}
+        [Required]
+        public string Topic {get;set;}
+        [Required]
+        [MinLength(10, ErrorMessage="Post must be 10 characters or longer!")]
+        public string Content {get;set;}
+    }
+}
